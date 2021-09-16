e9b7adb75996c558a40efa997f430acce04f4340
bigveloper <bigveloper@gmail.com>
2021-09-17 02:40:14 +0900

    commit add List

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import { useState } from 'react';
 function App() {
     // State
     const [value, setValue] = useState('');
+    const [list, setList] = useState([]);
 
     // Event
     const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+        if (!value) {
+            alert('text');
+            return;
+        }
+        setList((prevState) => [...prevState, value]);
+        setValue('');
+    };
     // JSX
     return (
         <div>
             <h1>To Do List</h1>
             <hr />
             <input value={value} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+            {list.map((item, index) => (
+                <div key={index}>{item}</div>
+            ))}
         </div>
     );
 }
