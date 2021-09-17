4f3e157fffe56c1a99dee8e1636cf0a2562bb92e
bigveloper <bigveloper@gmail.com>
2021-09-18 03:58:55 +0900

    ToDoList Add

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import { useState } from 'react';
 function App() {
     // State
     const [value, setValue] = useState('');
+    const [list, setList] = useState([]);
 
     // Event
     const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+        if (!value) {
+            alert('type Something');
+            return;
+        }
+        setList((prevState) => [...prevState, value]);
+        setValue('');
+    };
 
     // JSX
     return (
@@ -15,7 +24,11 @@ function App() {
             <h1> To Do List </h1>
             <hr />
             <input value={value} onChange={onChange} />
+            <button onClick={onClick}> Add </button>
             <hr />
+            {list.map((item, index) => (
+                <div>{item}</div>
+            ))}
         </div>
     );
 }
