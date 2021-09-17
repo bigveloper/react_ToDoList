d4ffd1a2cce0cf83942e76da914109ee0a4fe393
bigveloper <bigveloper@gmail.com>
2021-09-18 03:16:09 +0900

    ToDoList Add button & List

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 function App() {
     // State
     const [value, setValue] = useState('');
+    const [list, setList] = useState([]);
     // Event
     const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+        if (!value) {
+            alert('text');
+            return;
+        }
+        setList((prevState) => [...prevState, value]);
+        setValue('');
+    };
 
     // JSX(View)
     return (
@@ -13,6 +22,11 @@ function App() {
             <h1> To Do List </h1>
             <hr />
             <input value={value} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+
+            {list.map((item, index) => (
+                <div key={index}> {item} </div>
+            ))}
         </div>
     );
 }
