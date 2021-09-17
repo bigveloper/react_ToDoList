cef504826eefcabdd2fae76019853024d0562d57
bigveloper <bigveloper@gmail.com>
2021-09-18 03:51:01 +0900

    ToDoList input

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { useState } from 'react';
+
+// Component
+function App() {
+    // State
+    const [value, setValue] = useState('');
+
+    // Event
+    const onChange = (e) => setValue(e.target.value);
+
+    // JSX
+    return (
+        <div className="App">
+            <h1> To Do List </h1>
+            <hr />
+            <input value={value} onChange={onChange} />
+            <hr />
+        </div>
+    );
+}
+
+export default App;
