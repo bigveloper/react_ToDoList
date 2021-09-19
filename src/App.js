c2cc696980f65c94d07cc474b3e209b08f767a5f
bigveloper <bigveloper@gmail.com>
2021-09-19 22:42:35 +0900

    table third

--- src/App.js
+++ src/App.js
@@ -178,6 +178,7 @@ function App() {
                 <tr align="center" bgcolor="white">
                     <td rowSpan="2" bgcolor="red">
                         JavaScript
+                        <a href="https://developer.mozilla.org/">검색</a>
                     </td>
                     <td bgcolor="blue">Java</td>
                     <td bgcolor="green">TypeScript</td>
